d44d6d6580c6d6287208a8c2c3b244aba9ea92b5
Anton Taleiko <anton.taleiko@gmail.com>
2021-03-18 14:54:38 +0200

    1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,27 +6,43 @@ const Button = ({ handleClick, text }) => (
   </button>
 )
 
+const History = (props) => {
+  const { good, neutral, bad } = props
+  return (
+    <div>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const handleClick = () => {
-    console.log('clicked the button')
+  const handleGood = () => {
+    setGood(good + 1)
   }
 
-  const handleRightClick = () => {
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+  }
 
+  const handleBad = () => {
+    setBad(bad + 1)
   }
 
   return (
     <div>
       <h1>give feedback</h1>
-      <Button handleClick={handleClick} text="good" />
-      <Button handleClick={handleClick} text="neutral" />
-      <Button handleClick={handleClick} text="bad" />
+      <Button handleClick={handleGood} text="good" />
+      <Button handleClick={handleNeutral} text="neutral" />
+      <Button handleClick={handleBad} text="bad" />
       <h1>statistics</h1>
+      <History good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
