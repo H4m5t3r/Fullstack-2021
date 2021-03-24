ac92ebb0ef9b5efece1cfba9258e51fe3051fb60
Anton Taleiko <anton.taleiko@gmail.com>
2021-03-24 09:25:15 +0200

    2.15

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -35,9 +35,14 @@ const App = () => {
         name: newName,
         number: newNumber
       }
-      setPersons(persons.concat(personObject))
-      setNewName('')
-      setNewNumber('')
+      axios
+        .post('http://localhost:3001/persons', personObject)
+        .then(response => {
+          // console.log(response)
+          setPersons(persons.concat(personObject))
+          setNewName('')
+          setNewNumber('')
+        })
     } else {
       alert(`${newName} is already added to phonebook`)
     }
